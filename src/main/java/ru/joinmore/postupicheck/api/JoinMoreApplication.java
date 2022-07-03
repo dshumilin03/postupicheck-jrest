c13a81dfa72b6fd0b54022cfcb37647f40b5e8ac
dshumilin03 <55213131+dshumilin03@users.noreply.github.com>
2022-07-03 04:10:27 +0300

    submoduleTestCommit

--- src/main/java/ru/joinmore/postupicheck/api/JoinMoreApplication.java
+++ src/main/java/ru/joinmore/postupicheck/api/JoinMoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JoinMoreApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(JoinMoreApplication.class, args);
 	}
